9fb01602736afd1fc417db1efb2b38d0b2a0f7cf
soyoung1122 <xlor4528@gmail.com>
2023-08-08 10:12:08 +0900

    [ADD]unitPirce: 단가 기능추가

--- src/main/java/com/forEsther/service/unitpriceservice/UnitPriceServiceImpl.java
+++ src/main/java/com/forEsther/service/unitpriceservice/UnitPriceServiceImpl.java
@@ -1,14 +1,20 @@
 package com.forEsther.service.unitpriceservice;
 
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.interceptor.TransactionAspectSupport;
 
 import com.forEsther.mapper.unitpricemapper.UnitPriceMapper;
 import com.forEsther.vo.commonvo.Criteria;
+import com.forEsther.vo.quotationfilevo.QuotationFileVO;
 import com.forEsther.vo.unitpricevo.UnitPriceVO;
+
+
 
 @Service
 public class UnitPriceServiceImpl implements UnitPriceService {
@@ -21,6 +27,43 @@ public class UnitPriceServiceImpl implements UnitPriceService {
 		return mapper.insert(vo);
 	}
 
+	@Transactional(rollbackFor = {SQLException.class})
+	@Override
+	public int registerWithFile(UnitPriceVO unitPrice, QuotationFileVO file) {
+		try {
+			mapper.insert(unitPrice);
+			mapper.insertFile(file);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();
+			return 0;
+		}
+
+		return 1;
+	}
+
+	@Override
+	public String getThumbnail(String unit_price_code) {	
+		
+		String result = "";
+		
+		try {
+			result = mapper.getThumbnail(unit_price_code);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+		
+		return result;
+	}
+	
+	@Override
+	public List<Map<Integer, Integer>> getChart(String item_code) {
+		return mapper.getChart(item_code);
+	}
+	
+	
 	@Override
 	public int modify(UnitPriceVO vo) {
 		return mapper.update(vo);
@@ -45,6 +88,5 @@ public class UnitPriceServiceImpl implements UnitPriceService {
 	public List<Map<String, Object>> autocomplete(Map<String, Object> paramMap) {
 		return mapper.autocomplete(paramMap);
 	}
-	
 	
 }
