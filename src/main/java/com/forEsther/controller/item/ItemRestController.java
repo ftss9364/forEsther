77c868c59a5312281a36d1668ec6ae45ce6bde9e
ftss9364 <ftss9364@naver.com>
2023-08-31 17:42:04 +0900

    [ADD] 품목 RESTController 추가

--- src/main/java/com/forEsther/controller/item/ItemRestController.java
+++ src/main/java/com/forEsther/controller/item/ItemRestController.java
@@ -26,35 +26,42 @@ import lombok.extern.log4j.Log4j;
 @AllArgsConstructor
 @Log4j
 public class ItemRestController {
-	private ItemService service;
+   private ItemService service;
+   
 
-	@GetMapping("/detail")
-	public ItemVO getItemDetail(@RequestParam("itemCode") String itemCode) {
-		log.info("✅ [GET/Controller] 품목 상세 조회 ...");
-		return service.getItemDetail(itemCode);
-	}
-	
-	@PostMapping("/register")
-	public ResponseEntity<String> registerItem (@RequestBody JsonItemVO item ) {
-		log.info("[POST/Controller] 품목 등록 ...");
-		log.info(item);
-		int result = service.registerItem(item);
-		return result == 1 ? new ResponseEntity<String>("success", HttpStatus.OK)
+   @GetMapping("/detail")
+   public ItemVO getItemDetail(@RequestParam("itemCode") String itemCode) {
+      log.info("✅ [GET/Controller] 품목 상세 조회 ...");
+      log.info(itemCode);
+      return service.getItemDetail(itemCode);
+   }
+   
+   @PostMapping("/register")
+   public ResponseEntity<String> registerItem (@RequestBody JsonItemVO item ) {
+      log.info("[POST/Controller] 품목 등록 ...");
+      log.info(item);
+      int result = service.registerItem(item);
+      return result == 1 ? new ResponseEntity<String>("success", HttpStatus.OK)
                 : new ResponseEntity<String>(HttpStatus.INTERNAL_SERVER_ERROR);
-	}
-	
-	@PostMapping("/remove")
-	public ResponseEntity<String> removeItem(@RequestParam("itemCode") String itemCode) {
-		log.info("[POST/Controller] 품목 삭제 ...");
-		log.info(itemCode);
-		int result = service.removeItem(itemCode);
-		return result == 1 ? new ResponseEntity<String>("success", HttpStatus.OK)
+   }
+   
+   @PostMapping("/remove")
+   public ResponseEntity<String> removeItem(@RequestParam("itemCode") String itemCode) {
+      log.info("[POST/Controller] 품목 삭제 ...");
+      log.info(itemCode);
+      int result = service.removeItem(itemCode);
+      return result == 1 ? new ResponseEntity<String>("success", HttpStatus.OK)
                 : new ResponseEntity<String>(HttpStatus.INTERNAL_SERVER_ERROR);
-	}
-	
-//	@GetMapping("/modify")
-//	public ItemVO getItemModify(@RequestParam("itemCode") String itemCode) {
-//		log.info("✅ [GET/Controller] 수정할 상품 조회 ...");
-//		return service.getItemDetail(itemCode);
-//	}
+   }
+   
+//   ===================================================================================
+   
+   @GetMapping("/data")
+    public ResponseEntity<Object> getJsonData() {
+        List jsonData = service.getItemList();
+
+        log.info(jsonData);
+        // ResponseEntity를 사용하여 JSON 데이터와 HTTP 상태 코드를 반환한다.
+        return new ResponseEntity<>(jsonData, HttpStatus.OK);
+    }
 }
